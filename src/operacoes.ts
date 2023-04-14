720247a09a10bd9cf68645ea71f0cd6d50173407
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-04-14 19:50:59 -0300

    test: corrige teste operação aritmetica - multiplicação

--- src/operacoes.ts
+++ src/operacoes.ts
@@ -4,3 +4,7 @@ export const soma = (a: number, b: number): number => {
 export const subtracao = (a: number, b: number): number => {
   return a - b;
 };
+
+export const multiplicacao = (a: number, b: number): number => {
+  return a * b;
+};
--- tests/soma.spec.ts
+++ tests/soma.spec.ts
@@ -1,4 +1,5 @@
-import { soma, subtracao } from '../src/operacoes';
+import { soma, subtracao, multiplicacao } from '../src/operacoes';
+
 describe('testar operação aritmetica', () => {
   test('soma', () => {
     let resultado = soma(2, 1);
@@ -9,6 +10,12 @@ describe('testar operação aritmetica', () => {
   test('subtracao', () => {
     let resultado = subtracao(2, 1);
     let esperado = 1;
+    expect(resultado).toBe(esperado);
+  });
+
+  test('multiplicacao', () => {
+    let resultado = multiplicacao(2, 3);
+    let esperado = 6;
     expect(resultado).toBe(esperado);
   });
 });
